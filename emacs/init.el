76557fad13ea08be452f31674ea987d00ec7839a
Matt Copas <mcopas@scottlogic.com>
2021-12-14 15:46:10 +0000

    Fix logic for handling no installed Nerd Fonts

--- emacs/init.el
+++ emacs/init.el
@@ -29,7 +29,7 @@
 
 ;; Fonts
 (defun font-exists-p (font) "check if font exists" (if (null (x-list-fonts font)) nil t))
-(if (font-exists-p "MesloLGL NF") 
+(when (font-exists-p "MesloLGL NF") 
   (add-to-list 'default-frame-alist '(font . "MesloLGL NF" ))
   (set-face-attribute 'default t :font "MesloLGL NF" ))
 
