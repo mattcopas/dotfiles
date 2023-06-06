dc77b61b8c04de1c67e5543780589bf24959fd5b
Matt Copas <matt.copas@landregistry.gov.uk>
2023-06-06 09:23:22 +0100

    Create a composite agenda view based on Aaron Beiber's blog post

--- doom/config.el
+++ doom/config.el
@@ -85,9 +85,36 @@
   ;; The second parameter (t) disables the prompt to load a theme
   (load-theme 'doom-one t)
 
+  ;; Org agenda stuff
   ; Add all subdirectories in org-directory to org-agenda-files
   (setq org-agenda-files (f-directories org-directory nil t))
 
+  ;; This and the below custom commands come from Aaron Beiber's blog post -
+  ;; https://blog.aaronbieber.com/2016/09/24/an-agenda-for-life-with-org-mode.html
+  (defun air-org-skip-subtree-if-priority (priority)
+  "Skip an agenda subtree if it has a priority of PRIORITY.
+
+  PRIORITY may be one of the characters ?A, ?B, or ?C."
+    (let ((subtree-end (save-excursion (org-end-of-subtree t)))
+        (pri-value (* 1000 (- org-lowest-priority priority)))
+        (pri-current (org-get-priority (thing-at-point 'line t))))
+    (if (= pri-value pri-current)
+        subtree-end
+      nil)))
+
+  (setq org-agenda-custom-commands
+        '(("c" "Simple agenda view"
+           ((tags "PRIORITY=\"A\""
+                  ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                   (org-agenda-overriding-header "High priority unfinished tasks")))
+            (agenda "")
+            (alltodo ""
+                     ((org-agenda-skip-function
+                       '(or (air-org-skip-subtree-if-priority ?A)
+                            ; is the nil condition needed here?
+                            (org-agenda-skip-if nil '(scheduled deadline))))))))))
+
+  ;; Org capture stuff
   ; To see the original value of this (to see examples) comment this out and describe the variable
   (setq org-capture-templates '(("t" "Todo" entry (file+headline +org-capture-todo-file "Todo list")
                                 "* TODO %?")))
