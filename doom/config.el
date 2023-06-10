98ec4b88894f3baf7249b27ff66088c206166700
Matt <mattwfcopas@gmail.com>
2023-06-10 15:15:19 +0100

    extract dotfiles-doom-directory constant

--- doom/config.el
+++ doom/config.el
@@ -2,4 +2,5 @@
 ;; Note that the org file must NOT be called config.org
 ;; otherwise the org file gets tanged into config.el which causes an
 ;; infinite loop
-(org-babel-load-file "~/git/dotfiles/doom/configuration.org")
+(defconst dotfiles-doom-directory "~/git/dotfiles/doom/")
+(org-babel-load-file (concat dotfiles-doom-directory "configuration.org"))
