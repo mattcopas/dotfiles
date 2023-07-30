1b7630ee62971cc9170326a6ce666f50ad54861d
Matt Copas <mattwfcopas@gmail.com>
2023-07-30 14:41:45 +0100

    move neovim to its own folder

---
+++ neovim/link.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ 
+relative_directory=$(dirname "$0")
+dotfiles_directory=$(realpath $relative_directory)
+config_directory="$HOME/config/nvim"
+mkdir -p $config_directory
+echo "Creating symlink for $config_directory/init.vim to $dotfiles_directory/init.vim"
+# ln -s <real_file> <link>
+ln -s $dotfiles_directory/init.vim $config_directory/init.vim
