36639148702238e11b788f583bcfbefc6982e888
Matt Copas <mattwfcopas@gmail.com>
2023-08-25 19:53:07 +0100

    Add zshrc and create symlink

--- shell/link.sh
+++ shell/link.sh
@@ -1,23 +1,5 @@
-dotfiles_directory=$HOME/git/dotfiles
-shell_directory=$dotfiles_directory/shell
-rc_file="$HOME/.zshrc"
-
-echo "Sourcing dotfiles from $shell_directory to $rc_file"
-# Is there a better way to do this? zshrc tends to get modified across envs (usually for path stuff)
-# Perhaps zshrc should reference a path dotfile or something (if it exists)
-
-source_dotfile () {
-  local file=$1
-  local line_to_append="source $shell_directory/$file"
-  if [ $(grep -c "$line_to_append" "$rc_file") -gt 0 ] 
-  then
-    echo "Line '$line_to_append' already eists in $rc_file"
-  else
-    echo "Adding $file to $rc_file"
-    echo "$line_to_append" >> $rc_file
-  fi
-}
-
-source_dotfile alias
-source_dotfile functions
-source_dotfile env
+#!/bin/bash
+ 
+relative_directory=$(dirname "$0")
+dotfiles_directory=$(realpath $relative_directory)
+ln -s -f $dotfiles_directory/.zshrc $HOME/.zshrc
