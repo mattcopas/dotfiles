00a983b01316350019ae9204c6fa901da5376f9c
Matt Copas <matt.copas@landregistry.gov.uk>
2023-09-22 13:08:15 +0100

    update vanilla emacs launch script for emacs 29

--- emacs/vanilla-emacs.sh
+++ emacs/vanilla-emacs.sh
@@ -1,1 +1,11 @@
-emacs -nw -q --load init.el
+#! /bin/bash
+
+relative_directory=$(dirname "$0")
+script_directory=$(realpath $relative_directory)
+
+#
+# For older versions of emacs
+# emacs -nw -q --load $script_directory/init.el
+
+# Requires emacs 29+
+emacs --init-directory $script_directory
