a51f971953ad4ff328583ff2adb764636753fd08
Matt Copas <matthwfcopas@gmail.com>
2026-03-05 12:50:52 +0000

    Override the keymap <leader><leader> to search for files in a projet-aware fashion

--- neovim/nvim/lua/config/keymaps.lua
+++ neovim/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Change <leaser><Leadwr> to search for project aware files - instead of those in what ever the termninal $(pwd) is
+vim.keymap.set("n", "<leader><leader>", function()
+  LazyVim.pick("files")()
+end, { desc = "Find Files (Root Aware)" })
