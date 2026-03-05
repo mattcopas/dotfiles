4b9f098cea59bdcb26b65eb2ebe34ac431068fcd
Matt Copas <matthwfcopas@gmail.com>
2026-03-05 12:07:45 +0000

    add lazyvim for neovim and update link script

--- neovim/link.sh
+++ neovim/link.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
- 
+
 relative_directory=$(dirname "$0")
 dotfiles_directory=$(realpath $relative_directory)
 config_directory="$HOME/.config/nvim"
-mkdir -p $config_directory
-echo "Creating symlink for $config_directory/init.vim to $dotfiles_directory/init.vim"
+echo "Creating symlink for $config_directory to $dotfiles_directory/nvim"
 # ln -s <real_file> <link>
-ln -s -f $dotfiles_directory/init.vim $config_directory/init.vim
+ln -s -f $dotfiles_directory/nvim $config_directory
