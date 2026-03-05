b6882b560173de8f0a918ee5474bef98c1a4d6a4
Matt Copas <matthwfcopas@gmail.com>
2026-03-05 11:09:32 +0000

    fix neovim link script

--- neovim/link.sh
+++ neovim/link.sh
@@ -2,7 +2,7 @@
  
 relative_directory=$(dirname "$0")
 dotfiles_directory=$(realpath $relative_directory)
-config_directory="$HOME/config/nvim"
+config_directory="$HOME/.config/nvim"
 mkdir -p $config_directory
 echo "Creating symlink for $config_directory/init.vim to $dotfiles_directory/init.vim"
 # ln -s <real_file> <link>
