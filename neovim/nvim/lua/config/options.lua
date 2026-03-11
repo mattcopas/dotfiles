d41dd9fb566b6f0cc2ba03f8e7a5fd905f51662d
Matt Copas <matthwfcopas@gmail.com>
2026-03-11 14:38:47 +0000

    Add options to the correct place

--- neovim/nvim/lua/config/options.lua
+++ neovim/nvim/lua/config/options.lua
@@ -1,3 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+-- Don't show whitespace/tabs etc as underlines
+vim.opt.list = false
+
+-- Turn off formatting globally
+vim.g.autoformat = false
+
+-- Only underline warnings or worse
+vim.diagnostic.config({
+  underline = {
+    severity = {
+      min = vim.diagnostic.severity.WARN,
+    },
+  },
+})
