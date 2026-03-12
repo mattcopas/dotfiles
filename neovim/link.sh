a39c1db6999dfb08e69910a03226d4f2e879535c
Matt Copas <matthwfcopas@gmail.com>
2026-03-12 11:38:51 +0000

    add nvim-kickstart and use it

--- neovim/link.sh
+++ neovim/link.sh
@@ -5,4 +5,4 @@ dotfiles_directory=$(realpath $relative_directory)
 config_directory="$HOME/.config/nvim"
 echo "Creating symlink for $config_directory to $dotfiles_directory/nvim"
 # ln -s <real_file> <link>
-ln -s -f $dotfiles_directory/nvim $config_directory
+ln -s -f $dotfiles_directory/nvim-kickstart $config_directory
