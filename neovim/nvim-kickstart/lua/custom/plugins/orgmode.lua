f5115e556ba4d83a2d62849900f96f6280a95418
Matt Copas <matthwfcopas@gmail.com>
2026-07-15 09:07:32 +0100

    Neovim: Add Org Bullets, org mode specific keybinds, and org journal entries

--- neovim/nvim-kickstart/lua/custom/plugins/orgmode.lua
+++ neovim/nvim-kickstart/lua/custom/plugins/orgmode.lua
@@ -5,6 +5,7 @@
 --   Mixed: mixed =  {"a", "b", foo = "bar", baz = {"some", "array"} }  - access with mixed[1] or mixed.foo or mixed["foo"].
 -- So, we return a table (an array in this case) - which is made oup of mixed tables
 return {
+  -- Org Mode
   {
     'nvim-orgmode/orgmode',
     event = 'VeryLazy',
@@ -15,6 +16,9 @@ return {
       local personal_todo_file = '~/git/private-personal/todo.org'
       -- Setup orgmode
       require('orgmode').setup {
+        -- Unfortunately this calls some deprecated code, so this causes a crash atm - hopefully this will get fixed in a later version!
+        -- org_startup_indented = true
+        --
         -- TODO - update to what it was in emacs. CAn this be an array?
         org_agenda_files = {
           work_todo_file,
@@ -24,6 +28,17 @@ return {
         org_default_notes_file = '~/orgfiles/refile.org',
 
         org_capture_templates = {
+          -- Org Journal is just done with capture templates in Neovim
+          j = {
+            description = 'Journal Entry',
+            -- %<%b-%Y> dynamically resolves to the current year and month (e.g., Jul-2026.org)
+            target = '~/git/journal/%<%b-%Y>.org',
+            -- This formats the headline with today's date and time when you open it - eg
+            -- * Mon 13 July 2026
+            -- ** 17:35
+            -- The cursor goes below here (the %? in the template)
+            template = '* %<%a %-d %B %Y>\n** %<%H:%M>\n%?',
+          },
           p = {
             description = 'Personal Todo',
             template = '* TODO %?',
@@ -110,6 +125,41 @@ return {
       vim.lsp.enable 'org'
 
       vim.keymap.set('n', '<leader>X', '<cmd>Org capture<cr>', { desc = 'Org Capture Menu' })
+      vim.keymap.set('n', '<leader>njj', '<cmd>Org capture j<cr>', { desc = 'New Journal Entry' })
+
+      local org_keys = vim.api.nvim_create_augroup('OrgModeKeybindings', { clear = true })
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'org',
+        group = org_keys,
+        callback = function()
+          -- Force visual indentation mode on for this specific file buffer safely
+          -- Automatically turn on indentation, without org_startup_indented (see above re why that doesn't work)
+          vim.b.org_indent_mode = true
+
+          -- Org specific keybinds
+          vim.keymap.set('n', '<leader>mtd', '<cmd>OrgTodo DONE<cr>', { buffer = true, desc = 'Org: Mark DONE' })
+          vim.keymap.set('n', '<leader>mtt', '<cmd>OrgTodo TODO<cr>', { buffer = true, desc = 'Org: Mark TODO' })
+        end,
+      })
+    end,
+  },
+
+  -- Org Bullets
+  {
+    'nvim-orgmode/org-bullets.nvim',
+    event = 'VeryLazy',
+    ft = { 'org' },
+    dependencies = { 'nvim-orgmode/orgmode' }, -- Ensures core loads first
+    config = function()
+      require('org-bullets').setup {
+        concealcursor = false, -- If false, raw stars reappear on the exact line your cursor is on (very helpful for editing)
+        symbols = {
+          -- The icons used for Level 1, Level 2, Level 3 headings, etc.
+          headlines = { '◉', '○', '✸', '✿' },
+          -- The icon used for plain lists (e.g. - item)
+          list = '•',
+        },
+      }
     end,
   },
 }
