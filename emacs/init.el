d4e809127dc50ba77d1a39583532e2807636cc69
Matt Copas <mattwfcopas@gmail.com>
2026-08-04 21:24:15 +0100

    remove old vanilla emacs config

--- emacs/init.el
+++
@@ -1,3 +0,0 @@
-(defconst dotfiles-directory "~/git/dotfiles/")
-(defconst dotfiles-emacs-directory (concat dotfiles-directory "emacs/"))
-(org-babel-load-file (concat dotfiles-emacs-directory "configuration.org"))
