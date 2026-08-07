e0b53186f2f13fcc21ba3903af3e3d484a622414
Matt Copas <matthwfcopas@gmail.com>
2026-08-07 13:32:33 +0100

    enacs: add org captures and agendas (messily)

--- emacs/lisp/modules/org.el
+++ emacs/lisp/modules/org.el
@@ -10,11 +10,27 @@
   :ensure t ; Built-in to Emacs - BUT we want the latest version, so tell the package manager to download it anyway
   :defer t
   :hook (org-mode . me/org-mode-setup)
+  ;; Could do with moving this all into a file then calling that file in :config
+  :init
+  (setq me-private-directory "~/git/private-personal")
+  (setq me-private-personal-directory "~/git/private-personal")
+  (setq me-private-work-directory "~/git/private-work")
+  (setq me-private-client-directory "~/git/private-client")
+  (setq me-org-capture-todo-file (concat me-private-directory "/" ".todo.org"))
+  (setq me-org-capture-personal-todo-file (concat me-private-personal-directory "/" "todo.org"))
+  (setq me-org-capture-work-todo-file (concat me-private-work-directory "/" "todo.org"))
+  (setq me-org-capture-client-todo-file (concat me-private-client-directory "/" "notes.org"))
+  ;; Note we have to use backquote instead of quote here so that we can evaluate the variables before adding them to the list.
+  ;; See docs for function backquote
+  ;; Remove duplicates from the list - otherwise org agenda lists some tasks multiple times
+  (setq org-agenda-files (cl-remove-duplicates (list me-private-directory
+	    me-private-personal-directory
+	    me-private-client-directory
+	    me-private-work-directory) :test #'string-equal))
+
   :config
   (setq org-directory "~/git/"
         org-default-notes-file (expand-file-name "notes.org" org-directory)
-        org-agenda-files (list org-directory)
-        
         ;; Visual cosmetics & indentation
         org-ellipsis " [...] "
         org-hide-emphasis-markers t
@@ -86,8 +102,7 @@
    "oA" '(org-agenda :which-key "agenda")
    "X" '(org-capture :which-key "Org capture")
    "ol" '(org-insert-link :which-key "insert link")
-   "njj" '(org-journal-new-entry :which-key "New journal entry")
-   "oA" '(org-archive-subtree :which-key "archive subtree"))
+   "njj" '(org-journal-new-entry :which-key "New journal entry"))
 
   ;; Local keys inside Org buffers
   (general-define-key
@@ -113,6 +128,115 @@
    "mtd" '((lambda ()
 	     (interactive) (org-todo "DONE")) :which-key "Done")))
 
+;; Enable Habits - see https://orgmode.org/manual/Tracking-your-habits.html
+;(add-to-list 'org-modules 'habit)
+;; Don't log when habits are completed by default
+;; This can be changed for a specific todo item by setting the PROPERTY LOGGING -- see docs for org-log-repeat
+(setq org-log-repeat nil)
+
+; Don't make tasks with subtasks a different colour in org agenda
+; Is there a nicer way to mark tasks with subtasks in org agenda?
+(setq org-agenda-dim-blocked-tasks nil)
+
+;; Set this to t to get rid of the section lines
+(setq org-agenda-compact-blocks nil)
+
+;; This and the below custom commands come from Aaron Beiber's blog post -
+;; https://blog.aaronbieber.com/2016/09/24/an-agenda-for-life-with-org-mode.html
+;; TODO is it worth replacing this with the package 'org-super-agenda'?
+(defun air-org-skip-subtree-if-priority (priority)
+"Skip an agenda subtree if it has a priority of PRIORITY.
+
+PRIORITY may be one of the characters ?A, ?B, or ?C."
+  (let ((subtree-end (save-excursion (org-end-of-subtree t)))
+      (pri-value (* 1000 (- org-lowest-priority priority)))
+      (pri-current (org-get-priority (thing-at-point 'line t))))
+  (if (= pri-value pri-current)
+      subtree-end
+    nil)))
+
+(defun me/schedule-task-for (delta)
+  "Schedule a task for a given DELTA, for example \"+1d\" or \"-2d\"."
+  (cond ((equal major-mode 'org-mode) (org-schedule nil delta))
+        ((equal major-mode 'org-agenda-mode)
+         (org-agenda-schedule nil delta)
+         (org-agenda-redo))
+        ;; This is essentially the final else statement in an if/else-if block
+        (t (message (format "Invalid major-mode %s" major-mode)))))
+
+(defun me/schedule-task-for-today ()
+  "Schedule the task at point for today."
+  (interactive)
+  (me/schedule-task-for "+0d"))
+
+(defun me/schedule-task-for-tomorrow ()
+  "Schedule the task at point for tomorrow."
+  (interactive)
+  (me/schedule-task-for "+1d"))
+
+;; In org agenda, dont show subtasks by default. This can  be overridden for individual agenda views/sections -
+;; see the 'settings' section of the docs for org-agenda-custom-commands for details on overriding variables
+(setq org-tags-match-list-sublevels nil)
+
+(setq org-refile-use-outline-path 'full-file-path)
+
+(setq org-agenda-custom-commands
+      '(("c" "Simple agenda view"
+          ((tags "PRIORITY=\"A\""
+                ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                  (org-agenda-overriding-header "High priority unfinished tasks")))
+          (agenda "")
+          (alltodo ""
+                    ((org-agenda-skip-function
+                      '(or (air-org-skip-subtree-if-priority ?A)
+                          ; is the nil condition needed here?
+                          (org-agenda-skip-if nil '(scheduled deadline))))))))
+
+        ("w" "Work agenda view"
+                  ((tags "+work+PRIORITY=\"A\""
+                        ((org-agenda-skip-function '(org-agenda-skip-entry-if
+                                                     'todo '("WAIT" "KILL" "DONE")))
+                          (org-agenda-overriding-header "High priority unfinished tasks")))
+                  (tags-todo  "+work"
+                        ((org-agenda-skip-function '(org-agenda-skip-entry-if
+                                                     'todo '("TODO" "IN PROGRESS" "IN REVIEW" "KILL" "DONE")))
+                          (org-agenda-overriding-header "Blocked tasks")))
+                  (tags "+SCHEDULED=\"<today>\""
+                        ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("DONE" "KILL")))
+                         (org-agenda-overriding-header "Today's tasks")))
+                  (tags "+SCHEDULED=\"<today>\""
+                        ((org-agenda-skip-function '(org-agenda-skip-entry-if 'nottodo '("DONE")))
+                         (org-agenda-overriding-header "Today's completed tasks")))
+                  (tags "+SCHEDULED=\"<tomorrow>\""
+                        ((org-agenda-overriding-header "Tomorrow's tasks")))
+                  (tags-todo "+work"
+                            ((org-agenda-skip-function
+                              '(or (air-org-skip-subtree-if-priority ?A)
+                                   (org-agenda-skip-entry-if 'nottodo '("TODO" "IN PROGRESS"))))))))
+
+        ("p" "Personal agenda view"
+                  ((tags "+personal+PRIORITY=\"A\""
+                        ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                          (org-agenda-overriding-header "High priority unfinished tasks")))
+                  (tags-todo "+personal"
+                            ((org-agenda-skip-function
+                              '(or (air-org-skip-subtree-if-priority ?A)
+                                   (org-agenda-skip-entry-if 'todo 'done)))))))))
+
+(defun me-basic-todo-format ()
+  "Function returning a basic todo format. Note that the docs for org-capture-templates requirse the template parameter to be literal or a function returning a template"
+  "* TODO %?")
+
+(defun me-scheduled-todo-format-today ()
+  "Function returning a scheduled todo format (scheduled for today). Note that the docs for org-capture-templates requirse the template parameter to be literal or a function returning a template"
+  "* TODO %? \n SCHEDULED: %(org-insert-time-stamp (org-read-date nil t))")
+
+(setq org-capture-templates '(("t" "Todo" entry (file+headline me-org-capture-todo-file "Todo list") (function me-basic-todo-format))
+                              ("p" "Personal Todo" entry (file+headline me-org-capture-personal-todo-file "Todo list") (function me-basic-todo-format) :prepend t)
+                              ("w" "Work Todo" entry (file+headline me-org-capture-work-todo-file "Todo list") (function me-basic-todo-format) :prepend t)
+                              ("W" "Work Todo - today" entry (file+headline me-org-capture-work-todo-file "Todo list")
+                               (function me-scheduled-todo-format-today) :prepend t)
+                              ("c" "Client Todo" entry (file+headline me-org-capture-client-todo-file "Stuff to do") (function me-basic-todo-format) :prepend t)))
 
 (provide 'modules/org)
 ;;; org.el ends here
