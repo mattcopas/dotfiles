04a471526f3f28b40971487108330cd3614b364f
Matt Copas <matthwfcopas@gmail.com>
2026-08-28 09:55:50 +0100

    emacs: increase which-key delay

--- emacs/lisp/modules/which-key.el
+++ emacs/lisp/modules/which-key.el
@@ -10,8 +10,8 @@
 (use-package which-key
   :demand t
   :init
-  (setq which-key-idle-delay 0.3
-        which-key-idle-secondary-delay 0.05)
+  (setq which-key-idle-delay 0.7
+        which-key-idle-secondary-delay 0.7)
   :config
   (which-key-mode 1))
 
